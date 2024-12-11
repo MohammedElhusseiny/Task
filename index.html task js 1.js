abccbc47172a20ff0a86bd1de59cc06ff5c7fa3b
MohammedElhusseiny <mohammedelhusseiny97@gmail.com>
2024-12-11 13:41:15 +0200

    JS1

---
+++ index.html task js 1.js
@@ -0,0 +1,33 @@
+//function return a boolean if a number is divisible by 10
+
+/*function sayHellow (){
+
+    let data = +prompt("type the number")
+
+    if (data % 10 === 0){
+
+        console.log("true")
+
+    }else 
+
+    console.log("fales")
+}
+
+sayHellow()*/
+
+// great  function reverse an array
+
+
+/*let myFriend = ["ahmed","mohamed","sayed"]
+
+function arr (){
+
+    myFriend.reverse();
+
+    console.log(myFriend)
+
+}
+
+arr()*/
+
+
